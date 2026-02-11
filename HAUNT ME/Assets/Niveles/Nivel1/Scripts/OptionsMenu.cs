9e79cf8113e924c0d9726b43f119e090393ed991
CampusFP <sara.hernandezc@campusfp.es>
2026-02-11 11:18:59 +0100

    11//02/26

---
+++ HAUNT ME/Assets/Niveles/Nivel1/Scripts/OptionsMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class OptionsMenu : MonoBehaviour
+{
+    //nera
+ 
+    public void volver_a_inicio_2()
+    {
+        SceneManager.LoadScene("Inicio2");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+        EditorApplication.Exit(0);
+    }
+}
